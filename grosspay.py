213aac328db77db06f612dcadfbf4599cdea9507
rgi <rgi@rgis-MacBook-Pro.local>
2018-01-21 16:31:20 -0600

    added message

---
+++ grosspay.py
@@ -0,0 +1,9 @@
+#Reina Irizarry - A Program to Calculate Someone's Gross Pay
+pay_rate = 60.00
+hours_worked = 40.00
+gross_pay = pay_rate * hours_worked
+print ('Your Gross Pay is %.2f' %gross_pay)
+
+
+my_variable =
+print ('7')
